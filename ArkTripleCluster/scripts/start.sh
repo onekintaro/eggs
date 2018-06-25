7ea4fe5a4bdf6f01510cd12ed54dfcdf070ed3e4
onekintaro <kintaro221@gmail.com>
2018-06-25 23:23:48 +0200

    Update start.sh

--- ArkTripleCluster/scripts/start.sh
+++ ArkTripleCluster/scripts/start.sh
@@ -1,1 +1,2 @@
 #!/bin/bash
+./ShooterGame/Binaries/Linux/ShooterGameServer "Ragnarok?AltSaveDirectoryName=SaveRagnarok?Port=7777?QueryPort=27015?RCONEnabled=True?RCONPort=27020?ServerPVE=True?MaxPlayers=25" -ClusterDirOverride=/home/kintaro/Arkcluster -clusterid=swisscrafting1
