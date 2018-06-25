9674c2263e5e0dc0f74d91dde6de0a076d4bc3b8
onekintaro <kintaro221@gmail.com>
2018-06-25 23:19:39 +0200

    Update screen.sh

--- ArkTripleCluster/scripts/screen.sh
+++ ArkTripleCluster/scripts/screen.sh
@@ -1,1 +1,6 @@
 #!/bin/bash
+screen -dmS ark1 ./script.sh
+screen -dmS ark2 ./script.sh
+screen -dmS ark3 ./script.sh
+
+echo Server Started this may take a while until the server run. To view a server, just input screen -r ark[Number]
