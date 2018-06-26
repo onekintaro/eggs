2e8a79c85ffe4dac534a0f8efbad00df0c95b77d
onekintaro <kintaro221@gmail.com>
2018-06-26 20:18:10 +0200

    Update screen.sh

--- ArkTripleCluster/scripts/screen.sh
+++ ArkTripleCluster/scripts/screen.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-screen -dmS ark1 ./script.sh
-screen -dmS ark2 ./script.sh
-screen -dmS ark3 ./script.sh
+screen -dmS ark1 ./start.sh $1 $4 $7 $8 $9 ${10}
+screen -dmS ark2 ./start.sh $2 $5 $7 $8 $9 ${10}
+screen -dmS ark3 ./start.sh $3 $6 $7 $8 $9 ${10}
 
-echo Server Started this may take a while until the server run. To view a server, just input screen -r ark[Number]
+echo 'Server Started this may take a while until the server run. To view a server, just input screen -r ark[Number]'
