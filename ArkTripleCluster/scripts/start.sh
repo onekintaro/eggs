80257ad6dfc3b4306ed8e71ba81c2d23e400a9d3
onekintaro <kintaro221@gmail.com>
2018-06-26 20:40:04 +0200

    Update start.sh

--- ArkTripleCluster/scripts/start.sh
+++ ArkTripleCluster/scripts/start.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-#!/bin/bash
 screen -dmS ark1 "./Ark01/ShooterGame/Binaries/Linux/ShooterGameServer $1?listen?ServerAdminPassword=$4?Port=$5?MaxPlayers=$8" -ClusterDirOverride=/home/container/Arkcluster -clusterid=$9
 screen -dmS ark2 "./Ark02/ShooterGame/Binaries/Linux/ShooterGameServer $2?listen?ServerAdminPassword=$4?Port=$6?MaxPlayers=$8" -ClusterDirOverride=/home/container/Arkcluster -clusterid=$9
 screen -dmS ark3 "./Ark03/ShooterGame/Binaries/Linux/ShooterGameServer $3?listen?ServerAdminPassword=$4?Port=$7?MaxPlayers=$8" -ClusterDirOverride=/home/container/Arkcluster -clusterid=$9
