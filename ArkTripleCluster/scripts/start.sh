0c74f240a229ef0af181c8c834a5a737d614d39e
onekintaro <kintaro221@gmail.com>
2018-06-26 20:38:30 +0200

    Update start.sh

--- ArkTripleCluster/scripts/start.sh
+++ ArkTripleCluster/scripts/start.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
-./ShooterGame/Binaries/Linux/ShooterGameServer "Ragnarok?AltSaveDirectoryName=SaveRagnarok?Port=7777?QueryPort=27015?RCONEnabled=True?RCONPort=27020?ServerPVE=True?MaxPlayers=25" -ClusterDirOverride=/home/kintaro/Arkcluster -clusterid=swisscrafting1
+#!/bin/bash
+screen -dmS ark1 "./Ark01/ShooterGame/Binaries/Linux/ShooterGameServer $1?listen?ServerAdminPassword=$4?Port=$5?MaxPlayers=$8" -ClusterDirOverride=/home/container/Arkcluster -clusterid=$9
+screen -dmS ark2 "./Ark02/ShooterGame/Binaries/Linux/ShooterGameServer $2?listen?ServerAdminPassword=$4?Port=$6?MaxPlayers=$8" -ClusterDirOverride=/home/container/Arkcluster -clusterid=$9
+screen -dmS ark3 "./Ark03/ShooterGame/Binaries/Linux/ShooterGameServer $3?listen?ServerAdminPassword=$4?Port=$7?MaxPlayers=$8" -ClusterDirOverride=/home/container/Arkcluster -clusterid=$9
+
+echo 'Server Started this may take a while until the server run. To view a server, just input screen -r ark[Number]'
