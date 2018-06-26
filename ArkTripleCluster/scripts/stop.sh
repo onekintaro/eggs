09ad4bc627484c6700a242d11ab340cf4ca5bf40
onekintaro <kintaro221@gmail.com>
2018-06-26 21:04:02 +0200

    Update stop.sh

--- ArkTripleCluster/scripts/stop.sh
+++ ArkTripleCluster/scripts/stop.sh
@@ -1,2 +1,22 @@
 #!/bin/bash
-screen -X -S [session # you want to kill] quit
+host='127.0.0.1'
+port1=27020
+port2=27021
+port2=27022
+
+# rcon broadcast message
+/usr/bin/rcon -P$1 -a${host} -p${port1} 'broadcast This server is going offline in 15 seconds, the world is saving now, any changes made beyond this point will be lost.'
+/usr/bin/rcon -P$1 -a${host} -p${port2} 'broadcast This server is going offline in 15 seconds, the world is saving now, any changes made beyond this point will be lost.'
+/usr/bin/rcon -P$1 -a${host} -p${port3} 'broadcast This server is going offline in 15 seconds, the world is saving now, any changes made beyond this point will be lost.'
+
+# rcon saveworld
+/usr/bin/rcon -P$1 -a${host} -p${port1} 'saveworld'
+/usr/bin/rcon -P$1 -a${host} -p${port2} 'saveworld'
+/usr/bin/rcon -P$1 -a${host} -p${port3} 'saveworld'
+
+# wait 15s
+sleep 15;
+
+screen -X -S ark1 quit
+screen -X -S ark2 quit
+screen -X -S ark3 quit
