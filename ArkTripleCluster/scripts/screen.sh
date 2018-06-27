6c5ece6554fdd794d4a133235f3a6715b1ec2dac
onekintaro <kintaro221@gmail.com>
2018-06-27 14:24:03 +0200

    Delete screen.sh

--- ArkTripleCluster/scripts/screen.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-screen -dmS ark1 ./start.sh $1 $4 $7 $8 $9 ${10}
-screen -dmS ark2 ./start.sh $2 $5 $7 $8 $9 ${10}
-screen -dmS ark3 ./start.sh $3 $6 $7 $8 $9 ${10}
-
-echo 'Server Started this may take a while until the server run. To view a server, just input screen -r ark[Number]'
