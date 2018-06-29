fdf701602a0c16d3c50b68ef4621e167690f0755
onekintaro <kintaro221@gmail.com>
2018-06-29 21:54:52 +0200

    Update start.sh

--- ArkTripleCluster/scripts/start.sh
+++ ArkTripleCluster/scripts/start.sh
@@ -19,22 +19,22 @@ while true; do
         port2=27022
         
         # rcon broadcast message
-        /usr/bin/rcon -P$1 -a${host} -p${port1} 'broadcast This server is going offline in 15 seconds, the world is saving now, any changes made beyond this point will be lost.'
-        /usr/bin/rcon -P$1 -a${host} -p${port2} 'broadcast This server is going offline in 15 seconds, the world is saving now, any changes made beyond this point will be lost.'
-        /usr/bin/rcon -P$1 -a${host} -p${port3} 'broadcast This server is going offline in 15 seconds, the world is saving now, any changes made beyond this point will be lost.'
+        /usr/bin/rcon -P$4 -a${host} -p${port1} 'broadcast This server is going offline in 15 seconds, the world is saving now, any changes made beyond this point will be lost.'
+        /usr/bin/rcon -P$4 -a${host} -p${port2} 'broadcast This server is going offline in 15 seconds, the world is saving now, any changes made beyond this point will be lost.'
+        /usr/bin/rcon -P$4 -a${host} -p${port3} 'broadcast This server is going offline in 15 seconds, the world is saving now, any changes made beyond this point will be lost.'
         
         # rcon saveworld
-        /usr/bin/rcon -P$1 -a${host} -p${port1} 'saveworld'
-        /usr/bin/rcon -P$1 -a${host} -p${port2} 'saveworld'
-        /usr/bin/rcon -P$1 -a${host} -p${port3} 'saveworld'
+        /usr/bin/rcon -P$4 -a${host} -p${port1} 'saveworld'
+        /usr/bin/rcon -P$4 -a${host} -p${port2} 'saveworld'
+        /usr/bin/rcon -P$4 -a${host} -p${port3} 'saveworld'
         
         # wait 15s
         sleep 15;
         
         # rcon quit
-        /usr/bin/rcon -P$1 -a${host} -p${port1} 'DoExit'
-        /usr/bin/rcon -P$1 -a${host} -p${port2} 'DoExit'
-        /usr/bin/rcon -P$1 -a${host} -p${port3} 'DoExit'
+        /usr/bin/rcon -P$4 -a${host} -p${port1} 'DoExit'
+        /usr/bin/rcon -P$4 -a${host} -p${port2} 'DoExit'
+        /usr/bin/rcon -P$4 -a${host} -p${port3} 'DoExit'
         
         echo "Server Stopped"
         break
