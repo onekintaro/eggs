2fcd0315d6f0f86ef5b2125d52f4e689dd49372b
onekintaro <kintaro221@gmail.com>
2018-06-29 21:53:41 +0200

    Update start.sh

--- ArkTripleCluster/scripts/start.sh
+++ ArkTripleCluster/scripts/start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-/home/container/Ark01/ShooterGame/Binaries/Linux/ShooterGameServer "$1?listen?ServerAdminPassword=$4?Port=$5?MaxPlayers=$8?RCONEnabled=True?RCONPort=27020" -ClusterDirOverride=/home/container/Arkcluster -clusterid=$9 & \
-/home/container/Ark02/ShooterGame/Binaries/Linux/ShooterGameServer "$2?listen?ServerAdminPassword=$4?Port=$6?MaxPlayers=$8?RCONEnabled=True?RCONPort=27021" -ClusterDirOverride=/home/container/Arkcluster -clusterid=$9 & \
-/home/container/Ark03/ShooterGame/Binaries/Linux/ShooterGameServer "$3?listen?ServerAdminPassword=$4?Port=$7?MaxPlayers=$8?RCONEnabled=True?RCONPort=27022" -ClusterDirOverride=/home/container/Arkcluster -clusterid=$9 &
+/home/container/Ark01/ShooterGame/Binaries/Linux/ShooterGameServer "$1?listen?ServerAdminPassword=$4?Port=$5?MaxPlayers=$8?QueryPort=27015?RCONEnabled=True?RCONPort=27020" -ClusterDirOverride=/home/container/Arkcluster -clusterid=$9 & \
+/home/container/Ark02/ShooterGame/Binaries/Linux/ShooterGameServer "$2?listen?ServerAdminPassword=$4?Port=$6?MaxPlayers=$8?QueryPort=27016?RCONEnabled=True?RCONPort=27021" -ClusterDirOverride=/home/container/Arkcluster -clusterid=$9 & \
+/home/container/Ark03/ShooterGame/Binaries/Linux/ShooterGameServer "$3?listen?ServerAdminPassword=$4?Port=$7?MaxPlayers=$8?QueryPort=27017?RCONEnabled=True?RCONPort=27022" -ClusterDirOverride=/home/container/Arkcluster -clusterid=$9 &
 
 echo 'Server Started this may take a while until the server run.'
 
